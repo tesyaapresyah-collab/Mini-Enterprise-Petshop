d7d70d168972f17cac0221c0bc2682fd8e5365f8
tesya aprisah <tesyaapresyah@gmail.com>
2026-07-02 08:59:41 +0700

    Add mysql connector jar and test database connection

---
+++ Main.java
@@ -0,0 +1,8 @@
+import utils.DatabaseConnection;
+
+public class Main {
+    public static void main(String[] args) {
+        // Memanggil fungsi koneksi untuk tes
+        DatabaseConnection.getConnection();
+    }
+}
